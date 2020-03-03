ead38ee594174a15e0620b563ffd88bcc9bd65b1
Hessamoddin A Shokravi <ahs502@gmail.com>
2020-03-03 22:34:47 +0330

    Store auth actions

--- src/api/test.ts
+++ src/api/test.ts
@@ -3,14 +3,14 @@ const router = express.Router()
 
 export default router
 
-import store from '../store'
+// import store from '../store'
 
 router.get('/count', function (req, res, next) {
-  let count: number = store.count || 0
-  store.count = count = count + 1
+  // let count: number = store.count || 0
+  // store.count = count = count + 1
 
   res
-    .json({ result: count })
+    .json({ result: 0 })
     .status(200)
     .end()
 })
