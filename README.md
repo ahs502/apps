debfe668f25e98d54a2ee1a92c6124576ecd937d
Hessamoddin A Shokravi <ahs502@gmail.com>
2020-03-06 11:02:24 +0330

    Ability to override the default storage path

--- README.md
+++ README.md
@@ -4,9 +4,13 @@
 
     $ npm start
 
+You can use `STORAGE_PATH` to override the default storage path (`data` directory under the package root folder)
+
 ### Start the server for development:
 
     $ npm run develop
+
+You can use `STORAGE_PATH` to override the default storage path (`data` directory under the package root folder)
 
 ### Check for package updates:
 
--- src/store/kfs.ts
+++ src/store/kfs.ts
@@ -1,6 +1,9 @@
 import * as path from 'path'
 import keyFileStorage from 'key-file-storage'
 
-const kfs = keyFileStorage(path.join(__dirname, '../..', 'data'), false)
+const storagePath = process.env['STORAGE_PATH'] || path.join(__dirname, '../..', 'data')
+console.log('>> Storage path is'.yellow, storagePath.yellow.bold)
+
+const kfs = keyFileStorage(storagePath, false)
 
 export default kfs
