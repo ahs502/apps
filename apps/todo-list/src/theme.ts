79570b910ed04c8aaf5f09a71d693bda4b606e6b
Hessamoddin A Shokravi <ahs502@gmail.com>
2020-03-09 17:21:23 +0330

    Polish the default template and add Material UI

---
+++ apps/todo-list/src/theme.ts
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core'
+import { deepPurple, purple } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: deepPurple,
+    secondary: purple
+  }
+})
+
+export default theme
