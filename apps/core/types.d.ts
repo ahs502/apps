d90f3fc313b1d7a4dad47abf2c9fdc2d1c2003ed
Hessamoddin A Shokravi <ahs502@gmail.com>
2020-03-16 10:21:41 +0330

    (todo-list): Fix global type declarationss

--- apps/core/types.d.ts
+++ apps/core/types.d.ts
@@ -1,7 +1,8 @@
-declare const config: {
+declare interface Config {
   readonly app: string
   readonly loginUrl: string
 }
+declare const config: Config
 
 declare interface Persistant {
   'auth-code'?: string
--- apps/todo-list/src/types.d.ts
+++ apps/todo-list/src/types.d.ts
@@ -1,4 +1,4 @@
-declare const config: {
+declare interface Config {
   readonly x: number
 }
 
